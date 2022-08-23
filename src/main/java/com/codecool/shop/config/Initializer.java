558d3eb84551fcc074edc11b2fb21014c036c4d6
zsofi82 <zsofigaal@gmail.com>
2022-08-23 14:16:38 +0200

    add sample data: with details and optimise css to it

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -31,10 +31,12 @@ public class Initializer implements ServletContextListener {
         //setting up a new supplier
         Supplier houselander = new Supplier("HouseLander", "Bring you home from everywhere to everywhere safely.");
         supplierDataStore.add(houselander);
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier joker = new Supplier("Joker", "Make bad things");
+        supplierDataStore.add(joker);
+        Supplier scaryClown = new Supplier("ScaryClown", "Can scare you!");
+        supplierDataStore.add(scaryClown);
+        Supplier liberty = new Supplier("Liberty", "Save you from the trouble!");
+        supplierDataStore.add(liberty);
 
         //setting up a new product category
         ProductCategory sup = new ProductCategory("Superhero", "Family", "The perfect choice for everyone and for every services. Entertain families, save from and catches bad guys.");
@@ -45,12 +47,14 @@ public class Initializer implements ServletContextListener {
         //setting up a new superPower
         SuperPower fly = new SuperPower("Flyer", "Can fly");
         superPowerDataStore.add(fly);
+        SuperPower scare = new SuperPower("Scary", "Can scare you");
+        superPowerDataStore.add(fly);
 
         //setting up products and printing it
         productDataStore.add(new Product("Houselander", new BigDecimal("149.9"), "USD", "Great price with extra discount by DirtyDevs shoe store.", sup, houselander, fly));
-//        productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-//        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-//        productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Joker", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", sup, joker, scare));
+        productDataStore.add(new Product("ScaryClown", new BigDecimal("119"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", sup, scaryClown, scare));
+        productDataStore.add(new Product("Liberty", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", sup, liberty, fly));
         System.out.println(productDataStore.getAll());
     }
 }
