74c696d726bbd37a430cfbf3edd8881e92183ae9
Mark Palinkas <p.mark9807@gmail.com>
2022-08-23 13:51:06 +0200

    List products by category

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -31,24 +31,32 @@ public class Initializer implements ServletContextListener {
         //setting up a new supplier
         Supplier houselander = new Supplier("HouseLander", "Bring you home from everywhere to everywhere safely.");
         supplierDataStore.add(houselander);
+        Supplier houselander2 = new Supplier("HouseLander 2", "Bring you home from everywhere to everywhere safely.");
+        supplierDataStore.add(houselander2);
+        /*
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+         */
 
         //setting up a new product category
-        ProductCategory sup = new ProductCategory("Superhero", "Family", "The perfect choice for everyone and for every services. Entertain families, save from and catches bad guys.");
-        productCategoryDataStore.add(sup);
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory event = new ProductCategory("Event", "Family", "The perfect choice for everyone and for every services. Entertain families, save from and catches bad guys.");
+        productCategoryDataStore.add(event);
+        ProductCategory criminalJustice = new ProductCategory("Criminal justice", "Family", "The perfect choice for everyone and for every services. Entertain families, save from and catches bad guys.");
+        productCategoryDataStore.add(criminalJustice);
+        //ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        //productCategoryDataStore.add(tablet);
 
         //setting up a new superPower
         SuperPower fly = new SuperPower("Flyer", "Can fly");
         superPowerDataStore.add(fly);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Houselander", new BigDecimal("149.9"), "USD", "Great price with extra discount by DirtyDevs shoe store.", sup, houselander, fly));
-//        productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        productDataStore.add(new Product("Houselander", new BigDecimal("149.9"), "USD", "Great price with extra discount by DirtyDevs shoe store.", event, houselander, fly));
+        productDataStore.add(new Product("Houselander 2", new BigDecimal("149.9"), "USD", "Great price with extra discount by DirtyDevs shoe store.", criminalJustice, houselander2, fly));
+
+        //        productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
 //        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
 //        productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
         System.out.println(productDataStore.getAll());
