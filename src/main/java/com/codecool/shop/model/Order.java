53678c446ed83c3fd4faf7258ed112bd5eca9f11
csaszarattila42 <csaszarattila90@gmail.com>
2022-08-24 11:22:35 +0200

    feat: Create model for orders

---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,44 @@
+package com.codecool.shop.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order {
+    private final Map<Product, Integer> products;
+
+    public Order() {
+        products = new HashMap<>();
+    }
+
+    public void addProduct(Product product, int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException(String.format("Cant add zero or negative products: %d", quantity));
+        }
+        products.put(
+            product,
+            quantity + products.getOrDefault(product, 0)
+        );
+    }
+
+    public void addProduct(Product product) {
+        addProduct(product, 1);
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return Map.copyOf(products);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public void decreaseProduct(Product product, int quantity) {
+        if (products.containsKey(product)) {
+            if (products.get(product) <= quantity) {
+                removeProduct(product);
+            } else {
+                products.put(product, products.get(product) - quantity);
+            }
+        }
+    }
+}
